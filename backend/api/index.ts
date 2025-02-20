3e5657bccb9cfa648717f357dfc44e0a3430273c
darius molotokas <dariusm.it12@gmail.com>
2025-02-20 09:30:18 +0200

    fixed cookie issue

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -110,7 +110,7 @@ app.post("/login", (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
